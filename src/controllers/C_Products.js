8733f5f262e115650c6de427cdf5f3b10faeae2e
xRaksh <abhisheksha316@gmail.com>
2023-06-30 15:10:36 +0530

    Addding Filtration

--- src/controllers/C_Products.js
+++ src/controllers/C_Products.js
@@ -3,7 +3,7 @@ const Product = require("../models/M_produtcs")
 
 const getAllproducts = async(req,res) =>{
 
-    const {id,name} = req.query
+    const {id,name,Suggested,Sponsor,Recommed,Top} = req.query
     const queryObject = {}
     
     if(id){
@@ -12,10 +12,22 @@ const getAllproducts = async(req,res) =>{
     if(name){
         queryObject.name = { $regex:name, $options:"i" }
     }
+    if(Suggested == "true"){
+        queryObject.Suggested = Suggested
+    }
+    if(Sponsor == "true"){
+        queryObject.Sponsor = Sponsor
+    }
+    if(Recommed == "true"){
+        queryObject.Recommed = Recommed
+    }
+    if(Top == "ture"){
+        queryObject.Top = Top
+    }
 
     const Data = await Product.find(queryObject).sort('id -test');
     res.status(200).json({ Data })
-    console.log(Data);
+    // console.log(Data);
 }
 
 
